deac45b97e4275e7efb3719371913dc8742b0ff7
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-08-10 15:57:08 -0300

    controller Recipes pronta

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -12,48 +12,84 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("recipe")]
 public class RecipesController : ControllerBase
-{    
+{
     public readonly IRecipeService _service;
-    
+
     public RecipesController(IRecipeService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
-    // 1 - Sua aplicação deve ter o endpoint GET /recipe
-    //Read
+
     [HttpGet]
     public IActionResult Get()
     {
-        throw new NotImplementedException();    
+        var result = _service.GetRecipes();
+
+        return Ok(result);
     }
 
-    // 2 - Sua aplicação deve ter o endpoint GET /recipe/:name
-    //Read
+
     [HttpGet("{name}", Name = "GetRecipe")]
     public IActionResult Get(string name)
-    {                
-        throw new NotImplementedException();
+    {
+        var result = _service.GetRecipe(name);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
     }
 
     // 3 - Sua aplicação deve ter o endpoint POST /recipe
     [HttpPost]
-    public IActionResult Create([FromBody]Recipe recipe)
+    public IActionResult Create([FromBody] Recipe recipe)
     {
-        throw new NotImplementedException();
+        if (recipe == null)
+        {
+            return BadRequest();
+        }
+
+        _service.AddRecipe(recipe);
+
+        return Created("", recipe);
     }
+
 
     // 4 - Sua aplicação deve ter o endpoint PUT /recipe
     [HttpPut("{name}")]
-    public IActionResult Update(string name, [FromBody]Recipe recipe)
+    public IActionResult Update([FromBody] Recipe recipe)
     {
-        throw new NotImplementedException();
+
+        var recipeExists = _service.RecipeExists(recipe.Name);
+        if (recipeExists)
+        {
+            _service.UpdateRecipe(recipe);
+            return NoContent();
+        }
+
+        return BadRequest();
+
     }
 
+
+
     // 5 - Sua aplicação deve ter o endpoint DEL /recipe
+
     [HttpDelete("{name}")]
     public IActionResult Delete(string name)
     {
-        throw new NotImplementedException();
-    }    
+        var existingRecipe = _service.GetRecipe(name);
+
+        if (existingRecipe == null)
+        {
+            return NotFound();
+        }
+
+        _service.DeleteRecipe(name);
+
+        return NoContent();
+    }
 }
